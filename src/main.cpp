0598ee7da3951a76368ee56f10c21c44e9df3b93
Nahjay <nahjaybattieste@gmail.com>
2024-04-15 15:54:11 -0400

    feat: succcesfully wrote to device and defined communciation protocol

--- src/main.cpp
+++ src/main.cpp
@@ -6,34 +6,37 @@
 #define TFT_DC 9
 #define TFT_MOSI 11
 #define TFT_CLK 13
-#define TFT_MISO 12
-#define TFT_SCLK 13
-#define TFT_RST 255 // Reset pin not used
 
 // Create an instance of the display
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_CLK, TFT_MISO);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  // int result = myFunction(2, 3);
+  // Set up the display by beginning the SPI connection
+  SPI.setMOSI(TFT_MOSI);
+  SPI.setSCK(TFT_CLK);
 
-
+  // Initialize the display
+  tft.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // Print the result to the serial monitor
 
-  Serial.begin(9600);
-  Serial.println("Hello World!");
+  // Delay for 1 second
   delay(1000);
+
+  // Write hello world to the display
+  tft.setCursor(0, 0);
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(2);
+  tft.println("Hello World!");
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// } 
