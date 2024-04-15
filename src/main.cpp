140f0a97706a6287cdc2d8afe4a76a0e8f403a71
Nahjay <nahjaybattieste@gmail.com>
2024-04-15 15:43:38 -0400

    feat: defined pins and created an isntance of the display

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include <Arduino.h>
 #include <Adafruit_ILI9341.h>
 
+// Define the pins used for the display
+#define TFT_CS 10
+#define TFT_DC 9
+#define TFT_MOSI 11
+#define TFT_CLK 13
+#define TFT_MISO 12
+#define TFT_SCLK 13
+#define TFT_RST 255 // Reset pin not used
+
+// Create an instance of the display
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_CLK, TFT_MISO);
+
+
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // int result = myFunction(2, 3);
+
+
 }
 
 void loop() {
