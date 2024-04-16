92dc4398c6e23e79cac622a1e60664a871620a94
Nahjay <nahjaybattieste@gmail.com>
2024-04-16 16:08:27 -0400

    feat: massive rework of main to utilize new header inclusions

--- src/main.cpp
+++ src/main.cpp
@@ -27,34 +27,47 @@ double hit_sphere(const point3& center, double radius, const ray& r) {
   }
 }
 
-// Define the color function
-Color ray_color(const ray& r) {
-  // Shaded sphere
-  auto s = hit_sphere(point3(0,0,-1), 0.5, r);
-  if (s > 0.0) {
-    Vector3 N = unit_vector(r.at(s) - Vector3(0,0,-1));
-    return 0.5*Color(N.x()+1, N.y()+1, N.z()+1);
+// // Define the color function
+// Color ray_color(const ray& r) {
+//   // Shaded sphere
+//   auto s = hit_sphere(point3(0,0,-1), 0.5, r);
+//   if (s > 0.0) {
+//     Vector3 N = unit_vector(r.at(s) - Vector3(0,0,-1));
+//     return 0.5*Color(N.x()+1, N.y()+1, N.z()+1);
+//   }
+
+//   Vector3 unit_direction = unit_vector(r.direction());
+//   auto t = 0.5 * (unit_direction.y() + 1.0);
+//   Color color = (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
+
+//   Serial.print("Ray direction: ");
+//   Serial.print(r.direction().x());
+//   Serial.print(", ");
+//   Serial.print(r.direction().y());
+//   Serial.print(", ");
+//   Serial.println(r.direction().z());
+
+//   Serial.print("Color: ");
+//   Serial.print(color.x());
+//   Serial.print(", ");
+//   Serial.print(color.y());
+//   Serial.print(", ");
+//   Serial.println(color.z());
+
+//   return color;
+// }
+
+// Define the ray color function
+Color ray_color(const ray& r, const hittable& world) {
+  hit_record rec;
+
+  if (world.hit(r, 0, inf, rec)) {
+    return 0.5 * Color(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
   }
 
   Vector3 unit_direction = unit_vector(r.direction());
   auto t = 0.5 * (unit_direction.y() + 1.0);
-  Color color = (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
-
-  Serial.print("Ray direction: ");
-  Serial.print(r.direction().x());
-  Serial.print(", ");
-  Serial.print(r.direction().y());
-  Serial.print(", ");
-  Serial.println(r.direction().z());
-
-  Serial.print("Color: ");
-  Serial.print(color.x());
-  Serial.print(", ");
-  Serial.print(color.y());
-  Serial.print(", ");
-  Serial.println(color.z());
-
-  return color;
+  return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
 }
 
 void setup() {
@@ -90,6 +103,11 @@ void loop() {
   // Calculate the location of the upper left pixel should be 0, 0, 0
   auto viewport_upper_left = camera_origin - horizontal / 2 - vertical / 2 - Vector3(0, 0, 1);
 
+  // Create the World
+  hittable_list world;
+  world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
+  world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100));
+
   // Render the image
   for (int y = 0; y < tft.height(); y++) {
     for (int x = 0; x < tft.width(); x++) {
@@ -100,7 +118,7 @@ void loop() {
       ray r(camera_origin, viewport_upper_left + u * horizontal + v * vertical - camera_origin);
 
       // Get the color of the ray
-      Color pixel_color = ray_color(r);
+      Color pixel_color = ray_color(r, world);
 
       // Print the pixel color to the Serial Monitor
       Serial.print("Pixel color: ");
