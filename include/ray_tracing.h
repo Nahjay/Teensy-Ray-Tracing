07b5b8948ad8746cea581f0d5f9e64043b4d0ce0
Nahjay <nahjaybattieste@gmail.com>
2024-04-16 18:06:32 -0400

    feat: adding new random utlity functions to main header

--- include/ray_tracing.h
+++ include/ray_tracing.h
@@ -8,6 +8,7 @@
 #include <Adafruit_ILI9341.h>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 // Common Headers
 #include "vec3.h"
@@ -31,6 +32,14 @@ const double pi = 3.1415926535897932385;
 // Utility Functions
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max-min)*random_double();
 }
 
 #endif
