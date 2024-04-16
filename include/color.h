5ba8b0079f7c798961b2450fb4d06b244ff2a31f
Nahjay <nahjaybattieste@gmail.com>
2024-04-16 14:35:15 -0400

    fix: changign how write color works

--- include/color.h
+++ include/color.h
@@ -11,17 +11,29 @@
 // Define the color class using the Vector3 type as the underlying data type
 using Color = Vector3;
 
-void writeColor(Color pixelColor) {
+void writeColor(int x, int y, Color pixelColor, Adafruit_ILI9341 tft) {
   // Convert the color to 16-bit values
   uint16_t r = uint16_t(255.999 * pixelColor.x());
   uint16_t g = uint16_t(255.999 * pixelColor.y());
   uint16_t b = uint16_t(255.999 * pixelColor.z());
 
-  // Create an instance of the display
-  Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+  // Convert the RGB color to a 16-bit color
+  uint16_t color = tft.color565(r, g, b);
 
+  // Set the rotation of the display
+  tft.setRotation(3);
+
+  // Print the color to the Serial Monitor
+    Serial.print("Color: ");
+    Serial.print(r);
+    Serial.print(", ");
+    Serial.print(g);
+    Serial.print(", ");
+    Serial.println(b);
   // Write the color to the display
-  tft.writePixel(r, g, b);
+  tft.writePixel(x, y, color);
 }
+
+// Define a function to write the color to the display
 
 #endif 
--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,29 @@
 // Create an instance of the display
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
+// Define the color function
+Color ray_color(const ray& r) {
+  Vector3 unit_direction = unit_vector(r.direction());
+  auto t = 0.5 * (unit_direction.y() + 1.0);
+  Color color = (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
+
+  Serial.print("Ray direction: ");
+  Serial.print(r.direction().x());
+  Serial.print(", ");
+  Serial.print(r.direction().y());
+  Serial.print(", ");
+  Serial.println(r.direction().z());
+
+  Serial.print("Color: ");
+  Serial.print(color.x());
+  Serial.print(", ");
+  Serial.print(color.y());
+  Serial.print(", ");
+  Serial.println(color.z());
+
+  return color;
+}
+
 void setup() {
   // Set up the display by beginning the SPI connection
   SPI.setMOSI(TFT_MOSI);
@@ -26,7 +49,30 @@ void setup() {
 }
 
 void loop() {
+  // Clear the display
+  tft.fillScreen(ILI9341_BLACK);
 
+  // Display is 320x240
 
-  delay(1000000); // Delay to view the result for a while
+  // Define the image size to fit the display
+  auto aspect_ratio = tft.width() / tft.height();
+
+  // Camera
+  auto viewport_height = 2.0;
+  auto viewport_width = aspect_ratio * viewport_height;
+  auto focal_length = 1.0;
+  auto camera_origin = point3(0, 0, 0);
+
+  // Calculate the horizontal and vertical vectors
+  auto horizontal = Vector3(viewport_width, 0, 0);
+  auto vertical = Vector3(0, -viewport_height, 0);
+
+  // Calculate the location of the upper left pixel should be 0, 0, 0
+  auto viewport_upper_left = camera_origin;
+
+  
+
+  tft.fillScreen(ILI9341_BLUE);
+  
+  // delay(1000000); // Delay to view the result for a while
 }
