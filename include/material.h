c5d4456e926addeb4060f48496fb156c89783b07
Nahjay <nahjaybattieste@gmail.com>
2024-04-16 19:07:43 -0400

    feat: added new class to material header

--- include/material.h
+++ include/material.h
@@ -11,5 +11,19 @@ class material {
         virtual bool scatter(const ray& r_in, const hit_record& rec, Color& attenuation, ray& scattered) const = 0;
 };
 
+class lambertian : public material {
+    public:
+        lambertian(const Color& a) : albedo(a) {}
+
+        virtual bool scatter(const ray& r_in, const hit_record& rec, Color& attenuation, ray& scattered) const override {
+            Vector3 scatter_direction = rec.normal + random_unit_vector();
+            scattered = ray(rec.p, scatter_direction);
+            attenuation = albedo;
+            return true;
+        }
+
+    public:
+        Color albedo;
+};
 
 #endif
