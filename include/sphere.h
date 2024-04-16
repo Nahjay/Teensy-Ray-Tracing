84431e854e7d5e32fde4f9c540852055d30a68d2
Nahjay <nahjaybattieste@gmail.com>
2024-04-16 15:50:33 -0400

    wip: defined hit function

--- include/sphere.h
+++ include/sphere.h
@@ -8,6 +8,36 @@ class sphere : public hittable {
     public:
         sphere(const point3& cen, double rad) : center(cen), radius(fmax(0, rad)) {};
 
+    bool hit (const ray& r, double t_min, double t_max, hit_record& rec) const override {
+        Vector3 oc = r.origin() - center;
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius*radius;
+        auto discriminant = h*h - a*c;
+
+        if (discriminant < 0) {
+            return false;
+        }
+
+        auto sqrtd = sqrt(discriminant);
+
+        // Find the nearest root that lies in the acceptable range
+        auto root = (-h - sqrtd) / a;
+        if (root < t_min || t_max < root) {
+            root = (-h + sqrtd) / a;
+            if (root < t_min || t_max < root) {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        Vector3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+
+        return true;
+    }
+
     private:
         point3 center;
         double radius;
