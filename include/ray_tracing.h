f5ebc8adecaf812308cbaa46a0fb07a5e917bc6d
Nahjay <nahjaybattieste@gmail.com>
2024-04-16 15:59:55 -0400

    wip: defining general ray tracing header for better project encaspulation

---
+++ include/ray_tracing.h
@@ -0,0 +1,20 @@
+// General Header File
+
+#ifndef RAY_TRACING_H
+#define RAY_TRACING_H
+
+#include <cmath>
+#include <Arduino.h>
+#include <Adafruit_ILI9341.h>
+#include <limits>
+#include <memory>
+
+// Common Headers
+#include "vec3.h"
+#include "ray.h"
+#include "color.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
+#endif
