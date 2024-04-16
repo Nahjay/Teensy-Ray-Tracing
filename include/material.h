2f8d34eadf2684323bc8947d5671d04c1f82835c
Nahjay <nahjaybattieste@gmail.com>
2024-04-16 19:13:19 -0400

    wip: adding fuzziness to metal reflections

--- include/material.h
+++ include/material.h
@@ -32,4 +32,19 @@ class lambertian : public material {
         Color albedo;
 };
 
+class metal : public material {
+    public:
+        metal(const Color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
+
+        virtual bool scatter(const ray& r_in, const hit_record& rec, Color& attenuation, ray& scattered) const override {
+            Vector3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+            scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
+            attenuation = albedo;
+            return (dot(scattered.direction(), rec.normal) > 0);
+        }
+
+    public:
+        Color albedo;
+        double fuzz;
+};
 #endif
