e41bf65fbfa25ca62b005b5a6e8885e3562d9cec
Nahjay <nahjaybattieste@gmail.com>
2024-04-16 19:05:05 -0400

    wip: implementing material shared pointer for hit record class

--- include/hittable.h
+++ include/hittable.h
@@ -4,12 +4,14 @@
 #include "ray.h"
 #include "interval.h"
 
+class material;
 class hit_record {
     public:
         point3 p;
         Vector3 normal;
         double t;
         bool front_face;
+        std::shared_ptr<material> mat_ptr;
 
         inline void set_face_normal(const ray& r, const Vector3& outward_normal) {
             front_face = dot(r.direction(), outward_normal) < 0;
