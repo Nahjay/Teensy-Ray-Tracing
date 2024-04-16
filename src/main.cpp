46cb52ee24c597b4394aa8e0216a1d0981cfc8e2
Nahjay <nahjaybattieste@gmail.com>
2024-04-16 14:49:54 -0400

    wip: creating a hit sphere function

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,11 @@ Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 // Define the color function
 Color ray_color(const ray& r) {
+  // If the ray hits the sphere, return red
+  if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+    return Color(1, 0, 0);
+  }
+
   Vector3 unit_direction = unit_vector(r.direction());
   auto t = 0.5 * (unit_direction.y() + 1.0);
   Color color = (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);
@@ -34,6 +39,16 @@ Color ray_color(const ray& r) {
   Serial.println(color.z());
 
   return color;
+}
+
+// Defining a simple hit shpere function
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+  Vector3 oc = r.origin() - center;
+  auto a = dot(r.direction(), r.direction());
+  auto b = 2.0 * dot(oc, r.direction());
+  auto c = dot(oc, oc) - radius * radius;
+  auto discriminant = b*b - 4*a*c;
+  return (discriminant >= 0);
 }
 
 void setup() {
