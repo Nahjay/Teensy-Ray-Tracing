5a07a0557c2752388f53a8da566e8f88b74dc774
Nahjay <nahjaybattieste@gmail.com>
2024-04-17 11:40:09 -0400

    chore: better commenting out main ray tracing header file

--- include/ray_tracing.h
+++ include/ray_tracing.h
@@ -30,14 +30,18 @@ const double inf = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility Functions
+
+// Degrees to Radians
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+// Random Number Generation
 inline double random_double() {
     return rand() / (RAND_MAX + 1.0);
 }
 
+// Random Number Generation with Range
 inline double random_double(double min, double max) {
     return min + (max-min)*random_double();
 }
