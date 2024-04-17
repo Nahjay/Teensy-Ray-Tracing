53ff23a3fc45d8998a217add187d2da887d6cf01
Nahjay <nahjaybattieste@gmail.com>
2024-04-17 11:59:18 -0400

    chore: added comments for vector header file

--- include/vec3.h
+++ include/vec3.h
@@ -4,33 +4,42 @@
 #include <cmath>
 #include <Arduino.h>
 
+// Define the Vector3 class
 class Vector3 {
     public:
+
+    // Define the public variables
+
+    // The vector components
     double e[3];
 
+    // Define the constructors
     Vector3() : e{0, 0, 0} {}
     Vector3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
+    // Define the x, y, and z methods
     double x() const { return e[0]; }
     double y() const { return e[1]; }
     double z() const { return e[2]; }
-    // double random_double() {
-    //     return rand() / (RAND_MAX + 1.0);
-    // }
 
-
+    // Define the unary operators
     Vector3 operator-() const { return Vector3(-e[0], -e[1], -e[2]); }
     double operator[](int i) const { return e[i]; }
     double& operator[](int i) { return e[i]; }
 
+    // Define the vector arithmetic operators
     Vector3& operator+=(const Vector3 &v) {
+        // Add the vector to the current vector
         e[0] += v.e[0];
         e[1] += v.e[1];
         e[2] += v.e[2];
         return *this;
     }
 
+    // Define the vector arithmetic operators
+
     Vector3& operator*=(const double t) {
+        // Multiply the vector by the scalar
         e[0] *= t;
         e[1] *= t;
         e[2] *= t;
@@ -38,30 +47,39 @@ class Vector3 {
     }
 
     Vector3& operator/=(const double t) {
+        // Multiply the vector by the reciprocal of the scalar
         return *this *= 1/t;
     }
 
     double length() const {
+        // Return the length of the vector
         return sqrt(length_squared());
     }
 
     double length_squared() const {
+        // Return the squared length of the vector
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
 
+    // Utility Functions
+
     static double random_double() {
+        // Returns a random real in [0,1).
         return rand() / (RAND_MAX + 1.0);
     }
 
     static double random_double(double min, double max) {
+        // Returns a random real in [min,max).
         return min + (max-min)*random_double();
     }
 
     static Vector3 random() {
+        //  Returns a random vector with components in [0,1).
         return Vector3(random_double(), random_double(), random_double());
     }
 
     static Vector3 random(double min, double max) {
+        // Returns a random vector with components in [min,max).
         return Vector3(random_double(min, max), random_double(min, max), random_double(min, max));
     }
 
@@ -75,65 +93,59 @@ class Vector3 {
 // Create aliases for Vector3
 using point3 = Vector3;   // 3D point
 
-// Utility Functions
-
-// inline functions are functions that are defined in a header file and are meant to be inlined by the compiler.
-// Inlining is an optimization that eliminates the function call overhead by inserting the code of the function directly into the caller's code.
-// Inlining is not always beneficial, as it can increase the size of the compiled code.
-// In general, i is beneficial for small functions that are called frequently.
-
-// Inline function to output the vector using the Serial Monitor
-inline void outputVector(const Vector3& v) {
-    Serial.print("(");
-    Serial.print(v.x());
-    Serial.print(", ");
-    Serial.print(v.y());
-    Serial.print(", ");
-    Serial.print(v.z());
-    Serial.println(")");
-}
+// Inline Utility Functions
 
 inline Vector3 operator+(const Vector3 &u, const Vector3 &v) {
+    // Add two vectors
     return Vector3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
 inline Vector3 operator-(const Vector3 &u, const Vector3 &v) {
+    // Subtract two vectors
     return Vector3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
 inline Vector3 operator*(const Vector3 &u, const Vector3 &v) {
+    // Multiply two vectors
     return Vector3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
 inline Vector3 operator*(double t, const Vector3 &v) {
+    // Multiply a vector by a scalar
     return Vector3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
 inline Vector3 operator*(const Vector3 &v, double t) {
+    // Multiply a vector by a scalar
     return t * v;
 }
 
 inline Vector3 operator/(Vector3 v, double t) {
+    // Divide a vector by a scalar
     return (1/t) * v;
 }
 
 inline double dot(const Vector3 &u, const Vector3 &v) {
+    // Calculate the dot product of two vectors
     return u.e[0] * v.e[0]
          + u.e[1] * v.e[1]
          + u.e[2] * v.e[2];
 }
 
 inline Vector3 cross(const Vector3 &u, const Vector3 &v) {
+    // Calculate the cross product of two vectors
     return Vector3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
                    u.e[2] * v.e[0] - u.e[0] * v.e[2],
                    u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
 inline Vector3 unit_vector(Vector3 v) {
+    // Return the unit vector of the input vector
     return v / v.length();
 }
 
 inline Vector3 random_in_unit_sphere() {
+    // Returns a random vector in the unit sphere.
     while (true) {
         auto p = Vector3::random(-1, 1);
         if (p.length_squared() >= 1) continue;
@@ -142,10 +154,12 @@ inline Vector3 random_in_unit_sphere() {
 }
 
 inline Vector3 random_unit_vector() {
+    // Returns a random unit vector.
     return unit_vector(random_in_unit_sphere());
 }
 
 inline Vector3 random_in_unit_disk() {
+    // Returns a random vector in the unit disk.
     while (true) {
         auto p = Vector3(Vector3::random_double(-1,1), Vector3::random_double(-1,1), 0);
         if (p.length_squared() < 1)
@@ -154,6 +168,7 @@ inline Vector3 random_in_unit_disk() {
 }
 
 inline Vector3 random_on_hemisphere(const Vector3& normal) {
+    // Returns a random vector on the hemisphere.
     Vector3 in_unit_sphere = random_in_unit_sphere();
     if (dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
         return in_unit_sphere;
@@ -162,10 +177,12 @@ inline Vector3 random_on_hemisphere(const Vector3& normal) {
 }
 
 inline Vector3 reflect(const Vector3& v, const Vector3& n) {
+    // Reflect the vector v around the normal n
     return v - 2*dot(v,n)*n;
 }
 
 inline Vector3 refract(const Vector3& uv, const Vector3& n, double etai_over_etat) {
+    // Refract the vector uv around the normal n
     auto cos_theta = fmin(dot(-uv, n), 1.0);
     Vector3 r_out_perp = etai_over_etat * (uv + cos_theta*n);
     Vector3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
