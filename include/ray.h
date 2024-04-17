5dc6ad0b3aa14f87c244013579fc7e812b219e4a
Nahjay <nahjaybattieste@gmail.com>
2024-04-17 11:47:27 -0400

    chore: commented out ray  header

--- include/ray.h
+++ include/ray.h
@@ -3,21 +3,27 @@
 
 #include "vec3.h"
 
+// Define the ray class
 class ray {
+
+    // Define the public methods
     public:
+
+        // Define the constructors
         ray() {}
         ray(const point3& origin, const Vector3& direction)
             : orig(origin), dir(direction)
         {}
 
+        // Define the origin, direction, and at methods
         point3 origin() const  { return orig; }
         Vector3 direction() const { return dir; }
-
         point3 at(double t) const {
             return orig + t*dir;
         }
 
     public:
+        // Define the public variables
         point3 orig;
         Vector3 dir;
 };
