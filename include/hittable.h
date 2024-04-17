ccade6553da71fc7b97bfb585c5dbc7ec1fee054
Nahjay <nahjaybattieste@gmail.com>
2024-04-17 11:42:28 -0400

    chore: commented out hittable header file

--- include/hittable.h
+++ include/hittable.h
@@ -4,23 +4,38 @@
 #include "ray.h"
 #include "interval.h"
 
+
 class material;
+
+// Define the hit record class
 class hit_record {
+
+    // Define the public methods
     public:
+
+        // Define the constructors
         point3 p;
         Vector3 normal;
         double t;
         bool front_face;
         std::shared_ptr<material> mat_ptr;
 
+
+        // Define the set_face_normal method
         inline void set_face_normal(const ray& r, const Vector3& outward_normal) {
             front_face = dot(r.direction(), outward_normal) < 0;
             normal = front_face ? outward_normal : -outward_normal;
         }
 
 };
+
+// Define the hittable class
 class hittable {
+
+    // Define the public methods
     public:
+
+        // Define the hit method
         virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
 };
 
